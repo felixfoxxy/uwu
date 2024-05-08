8e82a052626c1cfdae22c728f0a3dfaeabee3a29
FelixFoxxy <81589714+felixfoxxy@users.noreply.github.com>
2024-05-08 15:28:30 +0200

    Add files via upload

---
+++ hyprset.sh
@@ -0,0 +1,13 @@
+paru -S hyprland-git hyprpicker-git waybar-git dunst nwg-look wf-recorder wlogout gammastep kitty hyprlock-git hypridle-git
+paru -S colord ffmpegthumbnailer gnome-keyring grimblast-git gtk-engine-murrine imagemagick kvantum pamixer polkit-kde-agent qt5-quickcontrols qt5-quickcontrols2 qt5-wayland qt6-wayland swww ttf-font-awesome tumbler ttf-jetbrains-mono ttf-icomoon-feather xdg-desktop-portal-hyprland-git xdotool xwaylandvideobridge-cursor-mode-2-git cliphist qt5-imageformats qt5ct swayosd-git
+git clone --depth=1 https://github.com/adi1090x/rofi.git
+cd rofi
+chmod +x setup.sh
+./setup.sh
+paru -S btop cava neofetch noise-suppression-for-voice rofi-lbonn-wayland-git rofi-emoji starship zsh viewnior ocs-url 
+paru -S firefox file-roller noto-fonts noto-fonts-cjk noto-fonts-emoji thunar thunar-archive-plugin
+paru -S code code-features code-marketplace
+paru -S papirus-icon-theme sddm-git pywal-git python-pywalfox python-pywayland swaylock-effects-git
+paru -S obs-studio ffmpeg-obs cef-minimal-obs-rc-bin pipewire pipewire-alsa pipewire-audio pipewire-pulse pipewire-jack wireplumber gst-plugin-pipewire pavucontrol
+paru -S pavuctl checkupdates gnome-system-monitor kweather psensor network-manager-applet
+hyprpm add https://github.com/hyprwm/hyprland-plugins
